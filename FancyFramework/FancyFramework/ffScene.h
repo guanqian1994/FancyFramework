cf869888b9e7f23b53e7589a360f894e5ed0e9ff
frimin <buzichang@vip.qq.com>
2013-12-19 17:05:55 +0800

    Signed-off-by: frimin <buzichang@vip.qq.com>

--- FancyFramework/FancyFramework/ffScene.h
+++ FancyFramework/FancyFramework/ffScene.h
@@ -29,7 +29,11 @@
 ////////////////////////////////////////////////////////////////////////////////
 #pragma once
 
+#include <vector>
 #include "ffApp.h"
+
+class ffLayer;
+typedef std::vector<ffLayer*> ffLayers;
 
 ////////////////////////////////////////////////////////////////////////////////
 /// @brief 场景类
@@ -54,16 +58,44 @@ public:
     /// @brief 画面更新时被调用
     virtual void OnRender(fDouble elapsedTime, f2dGraphics2D *pGraph);
 
-    /// @brief 处理接收的消息
+    /// @brief 处理消息
     /// @note 不可重写，上层消息首先将消息发送给此方法，如果消息没有经过处理则调用 OnMsg 供场景
     /// 来处理。若场景没有处理则交给框架来处理这个消息。
     bool HandleMsg(const f2dMsg &pMsg);
+
+    /// @brief 处理更新
+    bool HandleUpdate(fDouble elapsedTime);
+
+    /// @brief 处理渲染
+    bool HandleRender(fDouble elapsedTime, f2dGraphics2D *pGraph);
 
     /// @brief 当前场景是否已经被创建
     bool IsCreated();
 
     /// @brief 设置当前场景已经被初始化
     void SetCreated();
+
+    /// @brief 添加场景层
+    void AddLayer(ffLayer *pLayer);
+
+    /// @brief 移除场景层
+    void RemoveLayer(ffLayer *pLayer);
+
+    /// @brief 清楚所有场景层
+    void ClearLayers();
+
+    /// @brief 将场景层移至最前
+    void BringLayerToFront(ffLayer *pLayer);
+
+    /// @brief 将场景层移至最后
+    void BringLayerToBack(ffLayer *pLayer);
+
+    /// @brief 检查是否存在制定场景层
+    fBool HasLayer(ffLayer *pLayer);
+
+protected:
+
 private:
-    bool m_created;
+    fBool m_created;
+    ffLayers m_layers
 };
